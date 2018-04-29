9221c654f9e11f8097d3c02762ac259b433d8112
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-04-29 16:57:44 +0200

    added script for running

---
+++ run.sh
@@ -0,0 +1,2 @@
+rm -rf out
+./bin/hadoop jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/target/maven-hadoop-java-wordcount-template-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/points/ out/output 2
