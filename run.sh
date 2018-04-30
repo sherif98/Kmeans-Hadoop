f5d31e0ad720a67788293c7d801817fefd168395
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-04-30 18:59:12 +0200

    finished implementin kmeans with hadoop

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 rm -rf out
-/home/sherif/Desktop/hadoop-2.9.0/bin/hadoop jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/target/maven-hadoop-java-wordcount-template-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/points/ out/output 2
+time /home/sherif/Desktop/hadoop-2.9.0/bin/hadoop jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/target/maven-hadoop-java-wordcount-template-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Workspace/Java/wordcount-hadoop/maven-hadoop-java-wordcount-template/points/ out/output 6
--- src/main/java/com/example/Driver.java
+++ src/main/java/com/example/Driver.java
@@ -1,25 +1,3 @@
-/*
- * Driver.java
- *
- * Copyright 2014 Luca Menichetti <meniluca@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- *
- */
-
 package com.example;
 
 import org.apache.hadoop.conf.Configuration;
@@ -92,7 +70,7 @@ public class Driver extends Configured implements Tool {
             }
 
             Configuration conf = new Configuration();
-            Job job = Job.getInstance(conf, "Your job name");
+            Job job = Job.getInstance(conf, "Kmean");
             job.setJarByClass(Driver.class);
 
             job.setMapperClass(KMeanMapper.class);
